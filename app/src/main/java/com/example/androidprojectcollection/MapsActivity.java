458e2fb3fc8247aa3d419ed614943afef9a54ade
L11X07W12 <antonio.ubaldo@cit.edu>
2024-04-29 09:49:17 +0800

    [Hunyo] Maps Activity Updated 4/29/2024

--- app/src/main/java/com/example/androidprojectcollection/MapsActivity.java
+++ app/src/main/java/com/example/androidprojectcollection/MapsActivity.java
@@ -14,10 +14,12 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MapsActivity extends AppCompatActivity {
-    FrameLayout layoutBackground;
+    LinearLayout layoutBackground;
     LinearLayout layoutMaps;
+    TextView tvMapIndicator;
     ImageButton btnAkihabara, btnBoracay, btnPalawan, btnBaguio, btnEiffelTower;
 
     @Override
@@ -27,40 +29,41 @@ public class MapsActivity extends AppCompatActivity {
 
         layoutBackground = findViewById(R.id.layoutBackground);
         layoutMaps = findViewById(R.id.layoutMaps);
+        tvMapIndicator = findViewById(R.id.tvMapIndicator);
 
         btnAkihabara = findViewById(R.id.btnAkihabara);
         btnAkihabara.setOnClickListener(view -> {
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("geo:35.702210, 139.774145")));
-            layoutBackground.setBackground(btnAkihabara.getDrawable());
-            layoutMaps.setVisibility(View.GONE);
+            layoutBackground.setBackgroundResource(R.drawable.akihabaraview);
+            tvMapIndicator.setText("Akihabara Japan");
         });
 
         btnBoracay = findViewById(R.id.btnBoracay);
         btnBoracay.setOnClickListener(view -> {
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("geo:11.940653, 121.939211")));
-            layoutBackground.setBackground(btnBoracay.getDrawable());
-            layoutMaps.setVisibility(View.GONE);
+            layoutBackground.setBackgroundResource(R.drawable.boracayview);
+            tvMapIndicator.setText("Boracay Philippines");
         });
 
         btnPalawan = findViewById(R.id.btnPalawan);
         btnPalawan.setOnClickListener(view -> {
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("geo:12.069128, 120.157512")));
-            layoutBackground.setBackground(btnPalawan.getDrawable());
-            layoutMaps.setVisibility(View.GONE);
+            layoutBackground.setBackgroundResource(R.drawable.palawanview);
+            tvMapIndicator.setText("Palawan Philippines");
         });
 
         btnBaguio = findViewById(R.id.btnBaguio);
         btnBaguio.setOnClickListener(view -> {
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("geo:16.410045, 120.593598")));
-            layoutBackground.setBackground(btnBaguio.getDrawable());
-            layoutMaps.setVisibility(View.GONE);
+            layoutBackground.setBackgroundResource(R.drawable.baguioview);
+            tvMapIndicator.setText("Baguio Philippines");
         });
 
         btnEiffelTower = findViewById(R.id.btnEiffelTower);
         btnEiffelTower.setOnClickListener(view -> {
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("geo:48.857811, 2.295180")));
-            layoutBackground.setBackground(btnEiffelTower.getDrawable());
-            layoutMaps.setVisibility(View.GONE);
+            layoutBackground.setBackgroundResource(R.drawable.eiffeltowerview);
+            tvMapIndicator.setText("Eiffel Tower France");
         });
     }
 }
